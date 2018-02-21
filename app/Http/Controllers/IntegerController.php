225a16ab84fd574fc1f25296966cfa0426ebf952
testarray51 <flavius_ciprian9936@yahoo.com>
2018-02-22 00:49:08 +0200

    first commit
     Please enter the commit message for your changes. Lines starting

---
+++ app/Http/Controllers/IntegerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Integer;
+use Illuminate\Http\Request;
+
+class IntegerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Integer  $integer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Integer $integer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Integer  $integer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Integer $integer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Integer  $integer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Integer $integer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Integer  $integer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Integer $integer)
+    {
+        //
+    }
+}
---
+++ app/Transformers/IntegerTransformer.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pintican
+ * Date: 2/21/2018
+ * Time: 1:11 AM
+ */
+
+class IntegerTransformer
+{
+
+}
